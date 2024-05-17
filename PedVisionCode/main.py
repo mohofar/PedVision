aba22c2dbf64abdda709d588e78ba32b22796bc0
mohofar <mo.homayounfar@gmail.com>
2024-05-17 15:51:32 +0200

    feat: Add classifier_samples labels for training and validation

--- PedVisionCode/main.py
+++ PedVisionCode/main.py
@@ -8,7 +8,8 @@ from PedVisionCode.utils import annotation_roi
 from PedVisionCode.utils import train_roi_model
 from PedVisionCode.utils import VFM
 from PedVisionCode.utils import classifier_annotation
-
+from PedVisionCode.utils import train_cls_model
+from PedVisionCode.utils import HITL
 
 def main():
   # Create the argument parser
@@ -19,10 +20,15 @@ def main():
   parser.add_argument('--images_sampling', type=str, default='n', help=' y/n sample images from unlabelled_samples folder to ROI image folder and classifier image folder')
   parser.add_argument('--num_samp', type=int, default=2, help='Number of samples to be taken from unlabelled_samples folder for init round and annotation round')
   parser.add_argument('--ROI_annotation', type=str, default='n', help='y/n to run the ROI annotation framework')
-  parser.add_argument('--train_roi_model', type=str, default='n', help='y/n to train the ROI model')
-  parser.add_argument('--apply_FoM', type=str, default='n', help='y/n Apply Foundation model to get masks')
-
-  # parser.add_argument('--cls_annotation', type=str, help='y/n to run the classifier annotation framework')
+  parser.add_argument('--round', type=int, default=0, help='round number starting 0 to n')
+  parser.add_argument('--ROI_train', type=str, default='n', help='y/n to train the ROI model')
+  parser.add_argument('--apply_VFM', type=str, default='n', help='y/n Apply Foundation model to get masks')
+  parser.add_argument('--CLS_annotation', type=str, default='n', help='y/n to run the classifier annotation framework')
+  parser.add_argument('--CLS_train', type=str, default='n', help='y/n to train the classification model')
+  parser.add_argument('--HITL', type=str, default='n', help='y/n to run the Human-In-The-Loop framework')
+  parser.add_argument('--CLS_model_name', type=str, default='MobileNet', help='MobileNet, EffiB1, or EffiB5')
+  parser.add_argument('--HITL_num_samples', type=int, default=2, help='Number of samples to be taken from unlabelled_samples folder for HITL round')
+  parser.add_argument('--next_round_preparing', type=str, default='n', help='y/n to prepare for next round')
 
 
   # Parse the command-line arguments
@@ -38,17 +44,25 @@ def main():
   if args.ROI_annotation =='y':
     annotation_roi.main()
 
-  if args.train_roi_model =='y':
-    print('Training ROI model is running...')
-    train_roi_model.main()
+  if args.ROI_train =='y':
+    print('Training ROI model...')
+    train_roi_model.main(args.round)
 
   if args.apply_VFM =='y':
     print('VFM is running...')
-    VFM.main()
+    VFM.main(args.round)
   
-  # if args.cls_annotation =='y':
-  #   print('Classifier annotation is running...')
-  #   classifier_annotation.main()
+  if args.CLS_annotation =='y':
+    print('Classifier annotation is running...')
+    classifier_annotation.main()
+
+  if args.CLS_train =='y':
+    print('Training classifier model...')
+    train_cls_model.main(args.round)
+
+  if args.HITL =='y':
+    print('Human-In-The-Loop is running...')
+    HITL.main(args.HITL_num_samples, args.CLS_model_name, args.round)
 
 if __name__ == "__main__":
   main()
