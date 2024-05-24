24c758ed20e558012d92ac698e53a1094abb5f92
mohofar <mo.homayounfar@gmail.com>
2024-05-24 17:47:51 +0200

    chore: Move sample images to train folder

--- PedVisionCode/utils/sample_images.py
+++ PedVisionCode/utils/sample_images.py
@@ -5,10 +5,9 @@ import os
 
 def sample_images(n=10):
     # find all png images
-    images = [f for f in os.listdir('PedVisionCode/unlabelled_samples') if f.endswith('.png')]
-    for i in range(n):
-        image = random.choice(images)
-        shutil.move('PedVisionCode/unlabelled_samples/' + image, 'PedVisionCode/classifier_samples/images/train/' + image)
+    for i in os.listdir('PedVisionCode/unlabelled_samples/')[:n]:
+        shutil.move('PedVisionCode/unlabelled_samples/' + i, 'PedVisionCode/classifier_samples/images/train/' + i)
+
     for j in os.listdir('PedVisionCode/classifier_samples/images/train/'):
         shutil.copy('PedVisionCode/classifier_samples/images/train/' + j, 'PedVisionCode/ROI_samples/images/train/' + j)
 
