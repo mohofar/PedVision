73abd7bd734c0c0dd9f707c29578d28291d1faf9
Morteza Homayounfar <m.homayounfar@tudelft.nl>
2024-10-30 14:55:37 +0100

    Update test_cls_model.py

--- PedVisionCode/utils/test_cls_model.py
+++ PedVisionCode/utils/test_cls_model.py
@@ -6,87 +6,62 @@ from torchvision import models, transforms
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.metrics import classification_report, accuracy_score
 from glob import glob
-from torchvision import transforms
-import os
 from tqdm import tqdm
 import pickle
 import matplotlib.pyplot as plt
+import os
 
-def prepare_data_and_model_classifier(rounds, cls_num, model_name, img_name):
+def load_model_and_predict(mask, model_name, model_path, cls_num):
+    mask = torch.tensor(mask, dtype=torch.float32)
 
-
-    # Path to the directory containing the .npy files
-    directory_path = 'PedVisionCode/test_data/predicted/'
-
-    # Get a list of all final_image files
-    test_files = directory_path + 'VFM/for_cls_net_'+img_name+'.npy'
-
-    # def load_data(file):
-    #     image = np.load(file)
-    #     return image
-
-
-    # Load and concatenate data for training and validation sets
-    test_images = np.load(test_files)
-
-
-    # Convert data to PyTorch tensors
-    test_images = torch.tensor(test_images, dtype=torch.float32)
-
-    test_images = TensorDataset(test_images)
-    test_loader = DataLoader(test_images, batch_size=32, shuffle=True)
-
-    if(model_name == 'MobileNet'):
-        model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1)
-        model.classifier[1] = nn.Linear(model.last_channel, cls_num)
-    elif(model_name == 'EffiB1'):
-        model = models.efficientnet_b1(weights=models.EfficientNet_B1_Weights.IMAGENET1K_V1)
-        in_features = model.classifier[-1].in_features
-        model.classifier[-1] = nn.Linear(in_features, cls_num)
-    elif(model_name == 'EffiB5'):
-        model = models.efficientnet_b5(weights=models.EfficientNet_B5_Weights.IMAGENET1K_V1)
-        in_features = model.classifier[-1].in_features
-        model.classifier[-1] = nn.Linear(in_features, cls_num)
+    # Initialize model based on model_name
+    if model_name == 'MobileNet':
+        model_cls = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1)
+        model_cls.classifier[1] = nn.Linear(model_cls.last_channel, cls_num)
+    elif model_name == 'EffiB1':
+        model_cls = models.efficientnet_b1(weights=models.EfficientNet_B1_Weights.IMAGENET1K_V1)
+        model_cls.classifier[-1] = nn.Linear(model_cls.classifier[-1].in_features, cls_num)
+    elif model_name == 'EffiB5':
+        model_cls = models.efficientnet_b5(weights=models.EfficientNet_B5_Weights.IMAGENET1K_V1)
+        model_cls.classifier[-1] = nn.Linear(model_cls.classifier[-1].in_features, cls_num)
     else:
-        print('Specify the model name: MobileNet, EffiB1, or EffiB5')
+        raise ValueError("Specify model_name as 'MobileNet', 'EffiB1', or 'EffiB5'")
 
-    model.load_state_dict(torch.load('PedVisionCode/saved_models/CLS_model_R'+str(rounds)+'.pth'))
-    return test_loader, model
+    model_cls.load_state_dict(torch.load(model_path))
+    model_cls.eval()  # Set model to evaluation mode
+
+    # Predict
+    with torch.no_grad():
+        outputs = model_cls(mask)
+        _, predicted = torch.max(outputs, 1)
+
+    return predicted
+
 
 def main(rounds, cls_num, model_name, img_name, num_classes):
-    
-    test_loader, model = prepare_data_and_model_classifier(rounds, cls_num, model_name, img_name)
+    # File paths
+    directory_path = 'PedVisionCode/test_data/predicted/'
+    test_file_path = f'{directory_path}VFM/for_cls_net_{img_name}.npy'
+    pkl_file_path = f'{directory_path}VFM/org_mask_{img_name}.pkl'
 
-    # Evaluation
-    model.eval()
-    all_preds = []
-    with torch.no_grad():
-        for inputs in test_loader:
-            inputs = torch.stack(inputs)
-            outputs = model(inputs[0])
-            _, preds = torch.max(outputs, 1)
-            all_preds.extend(preds.numpy())
+    # Load image and mask data
+    test_images = np.load(test_file_path)
+    with open(pkl_file_path, 'rb') as f:
+        masks = pickle.load(f)
 
-    
-    with open('PedVisionCode/test_data/predicted/VFM/org_mask_'+img_name+'.pkl', 'rb') as f:
-        mask_np = pickle.load(f)
-    image_final = np.zeros((mask_np[0]['segmentation'].shape[0], mask_np[0]['segmentation'].shape[1], num_classes), dtype=np.bool_)
-    for i in range(len(mask_np)):
-      image_final[:,:,all_preds[i]] = image_final[:,:,all_preds[i]] + mask_np[i]['segmentation'].astype(np.bool_)
+    # Predict class for each mask
+    cls_model_path = f'PedVisionCode/saved_models/CLS_model_R{rounds}.pth'
+    prediction = load_model_and_predict(test_images, model_name, cls_model_path, cls_num)
 
-    print(image_final.shape)
-    plt.figure(figsize=(5,5))
-    for i in range(num_classes):
-        plt.subplot(1,num_classes,i+1)
-        plt.imshow(image_final[:,:,i])
-        plt.axis('off')
-        plt.title('Class '+str(i))
-    plt.tight_layout()
-    plt.savefig('PedVisionCode/test_data/prepared/'+img_name+"_pred.png", format="png", dpi=300)
+    # Display each class's mask
+    for bone in range(cls_num):
+        overall_mask = np.zeros(masks[0]['segmentation'].shape)
+        for i, pred in enumerate(prediction):
+            if pred.item() == bone:
+                overall_mask += masks[i]['segmentation']
+
+        plt.subplot(1, cls_num, bone + 1)
+        plt.imshow(overall_mask.astype(np.bool_), alpha=0.7)
+        plt.title(f'Class {bone}')
 
     plt.show()
-
-                    
-    
-# if __name__ == "__main__":
-#     main(rounds)
